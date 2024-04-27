8f304962f44e5bf1f93e93646e0497498a99c3d0
sh1Nome <shinodayo1024@gmail.com>
2024-04-28 04:40:21 +0900

    init

---
+++ shuffle-numbers/src/main/java/com/example/shufflenumbers/ShuffleNumbersApplication.java
@@ -0,0 +1,13 @@
+package com.example.shufflenumbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuffleNumbersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShuffleNumbersApplication.class, args);
+	}
+
+}
---
+++ shuffle-numbers/src/test/java/com/example/shufflenumbers/ShuffleNumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shufflenumbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuffleNumbersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
