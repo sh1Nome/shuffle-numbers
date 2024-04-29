77b5f6e21b9cda7329b48e60e63308874150633e
sh1Nome <shinodayo1024@gmail.com>
2024-04-29 20:53:54 +0900

    [update]modelAttribute

--- shuffle-numbers/src/main/java/com/example/shufflenumbers/controller/InputController.java
+++ shuffle-numbers/src/main/java/com/example/shufflenumbers/controller/InputController.java
@@ -1,14 +1,11 @@
 package com.example.shufflenumbers.controller;
 
-import java.util.ArrayList;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.example.shufflenumbers.form.InputList;
-import com.example.shufflenumbers.form.InputRecord;
 
 
 @Controller
@@ -16,11 +13,7 @@ public class InputController {
 
     @ModelAttribute
     public InputList setupForm() {
-        InputList inputList = new InputList();
-        inputList.setRecords(new ArrayList<>());
-        InputRecord inputRecord = new InputRecord();
-        inputList.getRecords().add(inputRecord);
-        return inputList;
+        return new InputList();
     }
 
     @GetMapping({"/", "input"})
